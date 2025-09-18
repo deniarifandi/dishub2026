b800c03f20a01c2158a9f8f4a7708d406d2c9811
deniarifandi <42629835+deniarifandi@users.noreply.github.com>
2025-09-18 16:51:09 +0700

    Update VaOwnerController.php

--- app/Controllers/VaOwnerController.php
+++ app/Controllers/VaOwnerController.php
@@ -66,7 +66,7 @@ class VaOwnerController extends BaseController
             'va_owner_berita_1'  => $this->request->getPost('va_owner_berita_1'),
             'va_owner_berita_2'  => $this->request->getPost('va_owner_berita_2'),
             'va_owner_berita_3'  => $this->request->getPost('va_owner_berita_3'),
-            'va_owner_hp'        => $this->request->getPost('va_owner_hp')
+            'va_owner_hp'        => $this->request->getPost('va_owner_hp'),
             'va_owner_expired'   => $tanggal
         ];
 
